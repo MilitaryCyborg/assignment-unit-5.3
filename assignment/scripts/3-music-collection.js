00c85d06a404e83c610b60c26ae8d762593dc93a
Dean McNevin <deaner1456@gmail.com>
2023-11-26 13:28:52 -0600

    added 6 collections of music

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,6 +3,7 @@ console.log('***** Music Collection *****')
 
 myCollection = [];
 
+//start addToCollection function
 function addToCollection(collection, title, artist, yearPublished) {
   const album = {
     title,
@@ -13,8 +14,30 @@ function addToCollection(collection, title, artist, yearPublished) {
   collection.push(album);
   return album;
 }
+//end addToCollection function
 
+//start of music collection
+let addedAlbum = addToCollection(myCollection, "Mystery", "Tale Of Us", 2019);
+console.log("Album added:", addedAlbum);
 
+let addedAlbum1 = addToCollection(myCollection, "In Chicago", "John Summit", 2022);
+console.log("Album added:", addedAlbum1);
+
+let addedAlbum2 = addToCollection(myCollection, "Thing Called Love", "Above & Beyond", 2010);
+console.log("Album added:", addedAlbum2);
+
+let addedAlbum3 = addToCollection(myCollection, "Blue Sky Action", "Above & Beyond", 2015);
+console.log("Album added:", addedAlbum3);
+
+let addedAlbum4 = addToCollection(myCollection, "Because You Move Me", "Tinlicker", 2022);
+console.log("Album added:", addedAlbum4);
+
+let addedAlbum5 = addToCollection(myCollection, "Fractal", "Tinlicker", 2020);
+console.log("Album added:", addedAlbum5);
+
+let addedAlbum6 = addToCollection(myCollection, "Heroes (we could be)", "Alesso", 2014);
+console.log("Album added:", addedAlbum6);
+//end of music collection
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
